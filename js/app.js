a1ed2b3a7ed341c37cb0c1326a410f682b889210
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-04 14:42:47 +0100

    [add]createTask and verify input

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ createApp({
     data(){
         return{
             errorString: '',
+            newMessageString: '',
             tasks:[
                 {
                     text:'Prendere il pane',
@@ -18,6 +19,19 @@ createApp({
                 this.tasks.splice(index, 1);
             }else{
                 this.errorString = 'Attenzione non puoi eliminare un promemoria se non l hai selezionato';
+            }
+        },
+        createTask(){
+            this.errorString='';
+            if(this.newMessageString.length <5){
+                this.errorString = 'Attenzione il promemoria deve avere una lunghezza minima di 5 caratteri';
+            }else{
+                const newTask={
+                    text: this.newMessageString,
+                    done:false
+                }
+                this.tasks.unshift(newTask);
+                this.newMessageString= '';
             }
         }
     },
